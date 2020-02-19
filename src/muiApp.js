7cd9009c0e1b59d06a66f8ea19757617f360a579
Sang Yum <sangyum@gmail.com>
2020-02-19 10:09:44 -0800

    set maxWidth to medium

--- src/muiApp.js
+++ src/muiApp.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 import Views from './views';
 
 const MuiApp = () => (
     <>
         <CssBaseline />
-        <Views />
+        <Container maxWidth="md">
+            <Views />
+        </Container>
     </>
 )
 
