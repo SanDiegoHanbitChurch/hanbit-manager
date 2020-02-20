3c30b6a0defd265113d19a4c9b1c805be130c9d8
Sang Yum <sangyum@gmail.com>
2020-02-19 17:03:23 -0800

    import chowon and mokjang

--- data/import.js
+++ data/import.js
@@ -11,6 +11,7 @@ firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
 const chowonList = require('./chowonList')
+const mokjangList = require('./mokjangList');
  
 // const result = excelToJson({
 //     sourceFile: 'addressbook.xlsx'
@@ -19,6 +20,17 @@ const chowonList = require('./chowonList')
 for (let i=0; i < chowonList.length; i++) {
 
   db.collection('chowon').add(chowonList[i])
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
+
+for (let i=0; i < mokjangList.length; i++) {
+
+  db.collection('mokjang').add(mokjangList[i])
     .then(function(docRef) {
       console.log("Document written with ID: ", docRef.id);
     })
---
+++ data/mokjangList.js
@@ -0,0 +1,244 @@
+const mokjangList = [
+    {
+        name: '나눔',
+        leader: '윤근희',
+        chowon: '느티나무'
+    },
+    {
+        name: '소망',
+        leader: '지명인',
+        chowon: '느티나무'
+    },
+    {
+        name: '헤세드',
+        leader: '박영하',
+        chowon: '느티나무'
+    },
+    {
+        name: '사랑',
+        leader: '박형준',
+        chowon: '느티나무'
+    },
+    {
+        name: '하늘향기',
+        leader: '주동식',
+        chown: '두나미스'
+    },
+    {
+        name: '예담',
+        leader: '조태유',
+        chown: '두나미스'
+    },
+    {
+        name: '참좋은',
+        leader: '이용인',
+        chown: '두나미스'
+    },
+    {
+        name: '은혜',
+        leader: '염상용',
+        chown: '두나미스'
+    },
+    {
+        name: '주남의 땅',
+        leader: '김형규',
+        chown: '두나미스'
+    },
+    {
+        name: '주의 처소',
+        leader: '강성민',
+        chown: '두나미스'
+    },
+    {
+        chowon: '바다',
+        name: '아름다운',
+        leader: '박군호'
+    },
+    {
+        chowon: '바다',
+        name: '감동',
+        leader: '이동희'
+    },
+    {
+        chowon: '바다',
+        name: '낮은 자의',
+        leader: '정진길'
+    },
+    {
+        chowon: '바다',
+        name: '축복의 통로',
+        leader: '박기수'
+    },
+    {
+        chowon: '바다',
+        name: '아라',
+        leader: '옥성민'
+    },
+    {
+        chowon: '바다',
+        name: '물댄동산',
+        leader: '신호순'
+    },
+    {
+        chowon: '베델',
+        name: '따뜻한',
+        leader: '이성창'
+    },
+    {
+        chowon: '베델',
+        name: '하나님의 정원',
+        leader: '최원하'
+    },
+    {
+        chowon: '베델',
+        name: '새벽이슬',
+        leader: '최영재'
+    },
+    {
+        chowon: '베델',
+        name: '디딤돌',
+        leader: '권창기'
+    },
+    {
+        chowon: '베델',
+        name: '예그리나',
+        leader: '김주일'
+    },
+    {
+        chowon: '베델',
+        name: '함께라면',
+        leader: '유영훈'
+    },
+    {
+        chowon: '비전',
+        name: '하늘바다',
+        leader: '김영택'
+    },
+    {
+        chowon: '비전',
+        name: '온유',
+        leader: '김재영'
+    },
+    {
+        chowon: '비전',
+        name: '미라클',
+        leader: '류현희'
+    },
+    {
+        chowon: '비전',
+        name: '발걸음',
+        leader: '양우석'
+    },
+    {
+        chowon: '비전',
+        name: '임마누엘',
+        leader: '박중성'
+    },
+    {
+        chowon: '빌레몬',
+        name: '그루터기',
+        leader: '양영준'
+    },
+    {
+        chowon: '빌레몬',
+        name: '즐거운',
+        leader: '박상빈'
+    },
+    {
+        chowon: '빌레몬',
+        name: '르우엘',
+        leader: '권성민'
+    },
+    {
+        chowon: '빌레몬',
+        name: '열매',
+        leader: '정준영'
+    },
+    {
+        chowon: '빌레몬',
+        name: '빌립보',
+        leader: '류재혁'
+    },
+    {
+        chowon: '빌레몬',
+        name: '드림',
+        leader: '박갑식'
+    },
+    {
+        chowon: '새로운',
+        name: '마레',
+        leader: '이기영'
+    },
+    {
+        chowon: '새로운',
+        name: '샘터',
+        leader: '이호섭'
+    },
+    {
+        chowon: '새로운',
+        name: '시온',
+        leader: '전태수'
+    },
+    {
+        chowon: '새로운',
+        name: '별밤',
+        leader: '이창환'
+    },
+    {
+        chowon: '새로운',
+        name: '어울림',
+        leader: '노성길'
+    },
+    {
+        chowon: '에바다',
+        name: '카라스',
+        leader: '김민호'
+    },
+    {
+        chowon: '에바다',
+        name: '시심나',
+        leader: '박철현'
+    },
+    {
+        chowon: '에바다',
+        name: '한마음',
+        leader: '최성애'
+    },
+    {
+        chowon: '에바다',
+        name: '샘물',
+        leader: '조태연'
+    },
+    {
+        chowon: '에바다',
+        name: '기도하는',
+        leader: '고재황'
+    },
+    {
+        chowon: '엘림',
+        name: '노아방주',
+        leader: '김병대'
+    },
+    {
+        chowon: '엘림',
+        name: '마라나타',
+        leader: '박성준'
+    },
+    {
+        chowon: '엘림',
+        name: '싯딤',
+        leader: '김준영'
+    },
+    {
+        chowon: '엘림',
+        name: '감사',
+        leader: '권대암'
+    },
+    {
+        chowon: '엘림',
+        name: '하늘빛',
+        leader: '원승현'
+    }
+]
+
+module.exports = mokjangList;
