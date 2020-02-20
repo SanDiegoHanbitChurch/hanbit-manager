16075d34e55fb3d4d26e979a787b55ee8352d54e
Sang Yum <sangyum@gmail.com>
2020-02-19 16:27:03 -0800

    added mokjang list

---
+++ data/chownList.js
@@ -0,0 +1,250 @@
+const chowonList = [
+    {
+        name: '느티나무',
+        leader: '이상식',
+        mokjangList: [
+            {
+                name: '나눔',
+                leader: ''
+            },
+            {
+                name: '소망',
+                leader: ''
+            },
+            {
+                name: '헤세드',
+                leader: ''
+            },
+            {
+                name: '사랑',
+                leader: ''
+            }
+        ]
+    },
+    {
+        name: '두나미스',
+        leader: '염상용',
+        mokjangList: [
+            {
+                name: '하늘향기',
+                leader: ''
+            },
+            {
+                name: '예담',
+                leader: ''
+            },
+            {
+                name: '참좋은',
+                leader: ''
+            },
+            {
+                name: '은혜',
+                leader: ''
+            },
+            {
+                name: '주남의 땅',
+                leader: ''
+            },
+            {
+                name: '주의 처소',
+                leader: ''
+            }
+        ]
+    },
+    {
+        name: '바다초원',
+        leader: '이봉천',
+        mokjangList: [
+            {
+                name: '아름다운',
+                leader: ''
+            },
+            {
+                name: '감동',
+                leader: ''
+            },
+            {
+                name: '낮은 자의',
+                leader: ''
+            },
+            {
+                name: '축복의 통로',
+                leader: ''
+            },
+            {
+                name: '알아',
+                leader: ''
+            },
+            {
+                name: '물댄동산',
+                leader: ''
+            }
+        ]
+    },
+    {
+        name: '베델',
+        leader: '이선재',
+        mokjangList: [
+            {
+                name: '따뜻한',
+                leader: ''
+            },
+            {
+                name: '하나님의 정원',
+                leader: ''
+            },
+            {
+                name: '새벽이슬',
+                leader: ''
+            },
+            {
+                name: '디딤돌',
+                leader: ''
+            },
+            {
+                name: '예그리나',
+                leader: ''
+            },
+            {
+                name: '함께라면',
+                leader: ''
+            }
+        ]
+    },
+    {
+        name: '비전',
+        leader: '최만호',
+        mokjangList: [
+            {
+                name: '하늘바다',
+                leader: ''
+            },
+            {
+                name: '온유',
+                leader: ''
+            },
+            {
+                name: '미라클',
+                leader: ''
+            },
+            {
+                name: '발걸음',
+                leader: ''
+            },
+            {
+                name: '임마누엘',
+                leader: ''
+            }
+        ]
+    },
+    {
+        name: '빌레몬',
+        leader: '황호인',
+        mokjangList: [
+            {
+                name: '그루터기',
+                leader: ''
+            },
+            {
+                name: '즐거운',
+                leader: ''
+            },
+            {
+                name: '르우엘',
+                leader: ''
+            },
+            {
+                name: '열매',
+                leader: ''
+            },
+            {
+                name: '빌립보',
+                leader: ''
+            },
+            {
+                name: '드림',
+                leader: ''
+            }
+        ]
+    },
+    {
+        name: '새로운',
+        leader: '신인완',
+        mokjangList: [
+            {
+                name: '마레',
+                leader: ''
+            },
+            {
+                name: '샘터',
+                leader: '이호섭'
+            },
+            {
+                name: '시온',
+                leader: ''
+            },
+            {
+                name: '별밤',
+                leader: ''
+            },
+            {
+                name: '어울림',
+                leader: ''
+            }
+        ]
+    },
+    {
+        name: '에바다',
+        leader: '박동훈',
+        mokjangList: [
+            {
+                name: '카라스',
+                leader: ''
+            },
+            {
+                name: '시심나',
+                leader: ''
+            },
+            {
+                name: '한마음',
+                leader: ''
+            },
+            {
+                name: '샘물',
+                leader: ''
+            },
+            {
+                name: '기도하는',
+                leader: ''
+            }
+        ]
+    },
+    {
+        name: '엘림',
+        leader: '이교태',
+        mokjangList: [
+            {
+                name: '노아방주',
+                leader: ''
+            },
+            {
+                name: '마라나타',
+                leader: ''
+            },
+            {
+                name: '싯딤',
+                leader: ''
+            },
+            {
+                name: '감사',
+                leader: ''
+            },
+            {
+                name: '하늘빛',
+                leader: ''
+            }
+        ]
+    }
+]
+
+export default chowonList;
