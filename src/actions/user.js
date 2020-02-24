863bfd77073bf7bd85ced07850651d879e1e95ec
Sang Yum <sangyum@gmail.com>
2020-02-24 15:53:11 -0800

    refactored user database actions

---
+++ src/actions/user.js
@@ -0,0 +1,47 @@
+import firebase from '../firebase';
+
+const db = firebase.firestore();
+
+const deleteUser = (user) => {
+    console.log('deleting user', user);
+    db.collection("user").doc(user.id).delete()
+        .then(() => console.log('delete user'));
+  };
+
+const updateUser = (user) => {
+
+    const {id, name, email, role, chowon, mokjang } = user;
+    const userToSave = {
+        id,
+        name,
+        email,
+        role
+    }
+
+    if (chowon) {
+        userToSave.chowon = chowon;
+    }
+    if (mokjang) {
+        userToSave.mokjang = mokjang;
+    }
+
+    if (role === '담임목사') {
+        delete userToSave.chowon;
+        delete userToSave.mokjang;
+    }
+
+    if (role === '초원장') {
+        delete userToSave.mokjang;
+    }
+    if (role === '목자') {
+        delete userToSave.chowon;
+    }
+
+    db.collection("user").doc(user.id).update(userToSave)
+        .then(doc => console.log('updated', doc));
+};
+
+export {
+    deleteUser,
+    updateUser
+}
--- src/views/admin/user/userList/index.js
+++ src/views/admin/user/userList/index.js
@@ -2,48 +2,9 @@ import React from 'react';
 import { FirestoreCollection } from 'react-firestore';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import UserList from './userList';
-import firebase from '../../../../firebase';
-
-const db = firebase.firestore();
+import { updateUser, deleteUser } from '../../../../actions/user';
 
 const UserListContainer = () => {
-
-  const deleteUser = (user) => {
-    console.log('deleting user', user);
-  };
-
-  const updateUser = (user) => {
-    console.log(user);
-    const {id, name, email, role, chowon, mokjang } = user;
-    const userToSave = {
-      id,
-      name,
-      email,
-      role
-    }
-  
-    if (chowon) {
-      userToSave.chowon = chowon;
-    }
-    if (mokjang) {
-      userToSave.mokjang = mokjang;
-    }
-
-    if (role === '담임목사') {
-      delete userToSave.chowon;
-      delete userToSave.mokjang;
-    }
-
-    if (role === '초원장') {
-      delete userToSave.mokjang;
-    }
-    if (role === '목자') {
-      delete userToSave.chowon;
-    }
-    
-    db.collection("user").doc(user.id).update(userToSave)
-      .then(doc => console.log('updated', doc));
-  };
 
   return (
     <FirestoreCollection 
