5316cacf020ff81419a7b394f062cea5746e0af8
Sang Yum <sangyum@gmail.com>
2020-02-26 07:15:28 -0800

    remove stage; updated firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,11 @@ const config = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
-};
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUAREMENT_ID
+  };
 
 module.exports = config;
