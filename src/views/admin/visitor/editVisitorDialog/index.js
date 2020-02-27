271526e1d02543581dc401061c0876598a42b279
Sang Yum <sangyum@gmail.com>
2020-02-27 10:31:55 -0800

    added dialog actions

--- src/views/admin/visitor/editVisitorDialog/index.js
+++ src/views/admin/visitor/editVisitorDialog/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { get } from 'lodash';
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions'
 import VisitorName from './visitorName';
 import FamilyMembers from './familyMembers';
 import ContactInfo from './contactInfo';
@@ -14,6 +16,18 @@ const EditVisitorDialog = ({open, handleSave, handleCancel, visitor = {}}) => {
   const [email, setEmail] = useState(get(visitor, 'members.0.email', ''));
   const [phoneNumber, setPhoneNumber] = useState(get(visitor, 'members.0.phoneNumber', ''));
   const [members, setMembers] = useState(get(visitor, 'members', []));
+
+  const disableSave = koreanName === '' && englishName === ''
+
+  const handleOnClickSave = () => {
+    handleSave({
+      koreanName,
+      englishName,
+      email,
+      phoneNumber,
+      members
+    })
+};
 
   return (
     <Dialog open={open} >
@@ -37,6 +51,14 @@ const EditVisitorDialog = ({open, handleSave, handleCancel, visitor = {}}) => {
           members={members} 
           setMembers={setMembers} />
       </DialogContent>
+      <DialogActions>
+          <Button onClick={handleCancel} color="primary">
+              Cancel
+          </Button>
+          <Button disabled={disableSave} onClick={handleOnClickSave} color="primary">
+              Save
+          </Button>
+      </DialogActions>
     </Dialog>
   )
 }
