923946986d64ee9a3e118c0be07e0fa77ec70525
Sang Yum <sangyum@gmail.com>
2020-02-28 13:14:21 -0800

    created search container

--- src/views/admin/family/familyList/index.js
+++ src/views/admin/family/familyList/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { TableContainer, Table, Paper, Box } from '@material-ui/core';
+import { TableContainer, Table, Paper } from '@material-ui/core';
 import Head from './head';
 import Body from './body';
-import SearchBar from '../../../shared/searchBar'
 
-const FamilyList = ({familyList, setSearchQuery}) => {
+const FamilyList = ({familyList}) => {
     
     return (
-        <>
-            <Box m={2}>
-                <SearchBar setSearchQuery={setSearchQuery} />
-            </Box>
-            <TableContainer component={Paper}>
-                <Table>
-                    <Head />
-                    <Body familyList={familyList} />
-                </Table>
-            </TableContainer>
-        </>
+        <TableContainer component={Paper}>
+            <Table>
+                <Head />
+                <Body familyList={familyList} />
+            </Table>
+        </TableContainer>
     )
 }
 
