26f98aac6a34356d031750763e2409042ad892f4
Jin Park <opmanual@gmail.com>
2020-02-28 15:28:48 -0800

    Added confirmation dialog

---
+++ src/stories/confirmationDialog.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import ConfirmationDialog from '../views/shared/confirmationDialog';
+
+export default {
+  title: 'Confirmation Dialog',
+  component: ConfirmationDialog
+}
+
+const handleYes = action('Yes');
+const handleNo = action('No');
+const open = true;
+
+export const defaultRendering = () => <ConfirmationDialog handleYes={handleYes} handleNo={handleNo} open={open} />
+export const closeDialog = () => <ConfirmationDialog handleYes={handleYes} handleNo={handleNo} open={false} />
---
+++ src/views/shared/confirmationDialog.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const ConfirmationDialog = ({handleNo, handleYes, open, user}) => {
+
+  return (
+    <div>
+      <Dialog open={open}>
+        <DialogTitle id="responsive-dialog-title">Delete User</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure to delete this user?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleNo} color="primary">
+            No
+          </Button>
+          <Button onClick={() => handleYes(user)} color="primary" autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
+
+export default ConfirmationDialog;
