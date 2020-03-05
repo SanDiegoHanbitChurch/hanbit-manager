a4958c42890cd25d4d2d998d00d2660ef9092998
Jin Park <opmanual@gmail.com>
2020-03-04 20:33:15 -0800

    Added no match dialog

---
+++ src/stories/NoMatchDialog.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import NoMatchDialog from '../views/shared/noMatchDialog';
+
+export default {
+  title:'shared/No Match Dialog',
+  component: NoMatchDialog
+}
+
+export const defaultRendering = () => <NoMatchDialog />
---
+++ src/views/shared/noMatchDialog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoMatchDialog = () => {
+  return (
+    <p>찾으시는 내용이 없습니다.</p>
+  )
+}
+
+export default NoMatchDialog;
