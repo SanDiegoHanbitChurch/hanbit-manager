292ac5aa51bab63015ed6b9a0aedaf732ee7030e
Jin Park <opmanual@gmail.com>
2020-03-24 22:14:40 -0700

    Made Chowon/Mokjang Clickable

--- src/views/shared/mokjangCard.js
+++ src/views/shared/mokjangCard.js
@@ -3,10 +3,9 @@ import { Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+import { CardActionArea } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 const useStyles = makeStyles(theme => ({
     card: {
@@ -38,18 +37,17 @@ const MokjangCard = ({ id, name, leader, chowon, avatar, memberFamilies = [], ed
     return (
         <Box m={1}>
             <Card classes={{ root: classes.card }}>
+              <CardActionArea
+                component={Link} to={`/mokjang/${name}`}
+              >
                 <CardHeader
                     avatar={
                         <Avatar alt={name} src={avatar || 'https://i.pravatar.cc/300'} className={classes.large}/>
                     }
-                    action={
-                        <IconButton component={Link} to={`/mokjang/${name}`}>
-                            <ArrowForwardIosIcon />
-                        </IconButton>
-                    }
                     title={`${name} (${chowon})`}
                     subheader={leader}
                 />
+              </CardActionArea>
             </Card>
         </Box>
     )
